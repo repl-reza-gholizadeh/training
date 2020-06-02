ad61b18c8abc0b6ae13fa74537bd181ac0d8f38f
repl-reza-gholizadeh <reza.gholizadeh@replicon.com>
2020-06-02 08:47:44 -0600

    var keyword (implicit typing)

--- asp-dot-net/c-sharp/gradebook/src/GradeBook/Program.cs
+++ asp-dot-net/c-sharp/gradebook/src/GradeBook/Program.cs
@@ -3,6 +3,10 @@
 namespace GradeBook {
     class Program {
         static void Main (string[] args) {
+            var x = 19.2;
+            double y = 2.2;
+            double result = x + y;
+            Console.WriteLine (result);
             if (args.Length > 0) {
                 //Console.WriteLine("Hello " + args[0] + "!");
                 Console.WriteLine ($"Hello {args[0]}!");
