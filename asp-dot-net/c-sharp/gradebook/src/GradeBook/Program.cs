b26cd2328fc2d87f5a0e9fdfdfbed36e83f28ffd
repl-reza-gholizadeh <reza.gholizadeh@replicon.com>
2020-06-02 08:57:57 -0600

    arrays

--- asp-dot-net/c-sharp/gradebook/src/GradeBook/Program.cs
+++ asp-dot-net/c-sharp/gradebook/src/GradeBook/Program.cs
@@ -3,9 +3,14 @@
 namespace GradeBook {
     class Program {
         static void Main (string[] args) {
-            var x = 19.2;
-            double y = 2.2;
-            double result = x + y;
+            double[] numbers = new double[3];
+            var someArray = new double[2] { 22, 1.2 };
+            numbers[0] = 1.2;
+            numbers[1] = 2.2;
+            numbers[2] = 3.3;
+            var result = numbers[0];
+            result += numbers[1];
+            result += numbers[2];
             Console.WriteLine (result);
             if (args.Length > 0) {
                 //Console.WriteLine("Hello " + args[0] + "!");
