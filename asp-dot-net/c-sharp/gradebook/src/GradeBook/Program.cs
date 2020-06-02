4aadb3de2d1223a80f3575d8c52ccc4fde2a8774
repl-reza-gholizadeh <reza.gholizadeh@replicon.com>
2020-06-02 09:08:47 -0600

    lists

--- asp-dot-net/c-sharp/gradebook/src/GradeBook/Program.cs
+++ asp-dot-net/c-sharp/gradebook/src/GradeBook/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook {
     class Program {
         static void Main (string[] args) {
-            var numbers = new double[3] { 1.3, 44.3, 19 };
+            var numbers = new double[] { 1.3, 44.3, 19 };
+            List<double> grades = new List<double> () { 1.3, 44.3, 19, 22 };
+            grades.Add (1.3);
             var result = 0.0;
-            foreach (double number in numbers) {
+            foreach (double number in grades) {
                 result += number;
             }
             Console.WriteLine (result);
